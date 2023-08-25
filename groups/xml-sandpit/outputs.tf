af6e5c6de599b8c28c623502112527fe6661d873
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2023-08-25 08:37:28 +0100

    Updated outputs

--- groups/xml-sandpit/outputs.tf
+++ groups/xml-sandpit/outputs.tf
@@ -1,4 +1,12 @@
-output "frontend_fqdn" {
+output "frontend_external_alb_name" {
+  value = var.alb_enable_external_access ? aws_lb.alb_external[0].dns_name : "N/A"
+}
+
+output "frontend_internal_alb_name" {
+  value = aws_lb.alb_internal.dns_name
+}
+
+output "frontend_internal_fqdn" {
   value = aws_route53_record.frontend_internal.fqdn
 }
 
